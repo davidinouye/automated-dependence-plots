a9e52ecd168309634e5f0770212712b9ce461e98
David Inouye <dinouye@purdue.edu>
2020-07-30 15:30:31 -0400

    Removed deprecated utils module

--- adp/utils.py
+++
@@ -1,5 +0,0 @@
-# Alias for `funcs` module but deprecated, please use `funcs` instead
-import warnings
-from .funcs import *
-warnings.warn('Please use `funcs` module instead of `utils` module. '
-              'Renamed to avoid confusion with `utility` module.')
